maciozik/sono-companion

commit 0d35a98d35dfa5809532f9c1ff83b8638f0bd948

refactor: use event options of tappable elements to trigger their logic
Options such as `click`, `pointerdown` or `pointerup` in the
`data-tappable` attribute were previously used to trigger the circle
animation only.

They are now also used to execute the business logic linked to the
tappable element through a unified `trigger` event.

index.html
@@ -29,7 +29,7 @@
 
         <h1>Sono Companion</h1>
 
-        <div id="settings-btn" data-load="settings" data-tappable=".4 250ms">
+        <div id="settings-btn" data-load="settings" data-tappable="pointerdown 0.4 250ms">
             <g-icon data-name="tune"></g-icon>
         </div>
 
@@ -104,7 +104,7 @@
             <section id="sonometer-controls" data-rows=7>
 
                 <!-- Reset button -->
-                <div class="sonometer-reset-btn tile grey disabled" data-tappable>
+                <div class="sonometer-reset-btn tile grey disabled" data-tappable="pointerup">
                     <g-icon data-name="settings_backup_restore"></g-icon>
                     <p class="label">Réinitialiser</p>
                 </div>
@@ -150,11 +150,11 @@
 
                 <!-- Decrease bpm -->
                 <div class="bpm-half">
-                    <div class="bpm-modifier-btn" data-modifier="/2" data-tappable="click">÷2</div>
+                    <div class="bpm-modifier-btn" data-modifier="/2" data-tappable>÷2</div>
                 </div>
                 <div class="bpm-dec">
-                    <div class="bpm-modifier-btn" data-modifier="-5" data-tappable="click">−5</div>
-                    <div class="bpm-modifier-btn" data-modifier="-1" data-tappable="click">−1</div>
+                    <div class="bpm-modifier-btn" data-modifier="-5" data-tappable>−5</div>
+                    <div class="bpm-modifier-btn" data-modifier="-1" data-tappable>−1</div>
                 </div>
 
                 <!-- Bpm wheel -->
@@ -167,11 +167,11 @@
 
                 <!-- Increase bpm -->
                 <div class="bpm-inc">
-                    <div class="bpm-modifier-btn" data-modifier="+5" data-tappable="click">+5</div>
-                    <div class="bpm-modifier-btn" data-modifier="+1" data-tappable="click">+1</div>
+                    <div class="bpm-modifier-btn" data-modifier="+5" data-tappable>+5</div>
+                    <div class="bpm-modifier-btn" data-modifier="+1" data-tappable>+1</div>
                 </div>
                 <div class="bpm-double">
-                    <div class="bpm-modifier-btn" data-modifier="x2" data-tappable="click">×2</div>
+                    <div class="bpm-modifier-btn" data-modifier="x2" data-tappable>×2</div>
                 </div>
 
             </section>
@@ -199,22 +199,21 @@
             <section id="tempo-controls" data-rows=4>
 
                 <!-- Replay button -->
-                <div class="metronome-replay-btn tile grey disabled" data-tappable>
+                <div class="metronome-replay-btn tile grey disabled" data-tappable="pointerdown">
                     <g-icon data-name="replay"></g-icon>
                     <p class="label">Relancer</p>
                 </div>
 
                 <!-- Play button -->
                 <div class="metronome-play">
-                    <!-- TODO Make on-pointer-down generic for both view event and tappable event -->
-                    <div class="play-btn adaptative on-pointer-down" data-tappable>
+                    <div class="play-btn adaptative" data-tappable="pointerdown">
                         <g-icon data-name="play_arrow" data-x=-1 class="play"></g-icon>
                         <g-icon data-name="stop" class="stop"></g-icon>
                     </div>
                 </div>
 
                 <!-- Tap Tempo button -->
-                <div class="tap-tempo-btn tile grey" data-tappable>
+                <div class="tap-tempo-btn tile grey" data-tappable="pointerdown">
                     <g-icon data-name="touch_app"></g-icon>
                     <p class="label">Tap Tempo</p>
                 </div>
@@ -499,8 +498,8 @@
             <div class="modal-title"></div>
             <div class="modal-text"></div>
             <div class="modal-btns">
-                <div class="modal-btn-secondary btn btn-no-fill" data-tappable="click"></div>
-                <div class="modal-btn-primary btn btn-no-fill" data-tappable="click"></div>
+                <div class="modal-btn-secondary btn btn-no-fill" data-tappable></div>
+                <div class="modal-btn-primary btn btn-no-fill" data-tappable></div>
             </div>
         </div>
     </div>
@@ -523,7 +522,7 @@
         </div>
         <div class="too-long-msg">
             Le chargement prend plus de temps que prévu.<br>
-            <div class="btn btn-no-fill">Réessayer</div>
+            <div class="btn btn-no-fill" data-tappable="0.02 300ms">Réessayer</div>
         </div>
     </div>
 

js/modules/settings.js
@@ -299,7 +299,7 @@ export function __init__({})
     for (const $setting of $settings) {
 
         // Click on a setting.
-        $setting.addEventListener('click', function () {
+        $setting.addEventListener('trigger', function () {
             this.trigger();
         });
 
@@ -318,7 +318,7 @@ export function __init__({})
     // Click on a reset button of a setting.
     document.querySelectorAll('.setting .reset-btn').forEach($resetBtn => {
 
-        $resetBtn.addEventListener('click', function (event) {
+        $resetBtn.addEventListener('trigger', function (event) {
             const $setting = this.closest('.setting');
             $setting.reset();
             event.stopPropagation()

js/modules/utils/tappable.js
@@ -1,20 +1,22 @@
 /**
  * @description
- * A tappable is an element that trigger a circle growing animation when pressed.
+ * A tappable is an interactive element that trigger a growing circle animation when pressed.
  * It is defined by the presence of the 'data-tappable' attribute.
  *
  * Several options can be defined in this attribute, separated by white spaces:
- *  - `pointerdown`     : Trigger the animation on pointerdown event.
- *  - `pointerup`       : Trigger the animation on pointerup event.
- *  - `click`           : Trigger the animation on click event.
- *  - `trigger-manually`: The animation will not be triggered by an event and must be triggered manually.
- *  - `follow-tap`      : The animation is triggered at the position of the tap.
+ *  - `click`           : Trigger the element on click event (default).
+ *  - `pointerdown`     : Trigger the element on pointerdown event.
+ *  - `pointerup`       : Trigger the element on pointerup event.
+ *  - `trigger-manually`: The element will not be triggered by an event and must be triggered manually.
+ *  - `follow-tap`      : The circle animation is triggered at the position of the tap.
+ *  - `no-circle`       : The circle animation will not be shown.
  *  - {Any raw number}  : The opacity of the circle (ie. 0.2).
  *  - {Any duration}    : The animation duration of the circle (ie. 200ms, 1s).
  *
  * Moreover, the following classes can change the behavior of the element:
  *  - `disabled`        : The animation will not be triggered at all.
- *  - `cancel-tappable` : If present on a child element, any action targeting that element or their descendants will no longer trigger the animation.
+ *  - `cancel-tappable` : If present on a child element, any action targeting that element or their descendants
+ *                        will no longer trigger the animation of the parent.
  */
 
 const $tappables = document.querySelectorAll('[data-tappable]');
@@ -46,6 +48,7 @@ for (const $tappable of $tappables) {
     /**
      * Trigger the animation.
      * @param {Event} event
+     * @fires `trigger` on $tappable.
      */
     $tappable.triggerTappable = function (event) {
 
@@ -69,14 +72,17 @@ for (const $tappable of $tappables) {
 
             // Run the animation.
             this.classList.add('ontap');
+
+            // Emit the 'trigger' event.
+            this.dispatchEvent(new CustomEvent('trigger'));
         }
     }
 
     // Define the type of event.
-    let event_type = 'pointerdown';
+    let event_type = 'click';
 
-    if ($tappable.is('click')) {
-        event_type = 'click';
+    if ($tappable.is('pointerdown')) {
+        event_type = 'pointerdown';
     }
     else if ($tappable.is('pointerup')) {
         event_type = 'pointerup';

css/components/loader.css
@@ -71,6 +71,10 @@
         font-weight: 500;
         text-align: center;
 
+        .btn {
+            justify-self: center;
+        }
+
         &.show {
             display: block;
         }

css/components/tappable.css
@@ -44,6 +44,12 @@
         }
     }
 
+    &[data-tappable~=no-circle] {
+        &:before {
+            display: none;
+        }
+    }
+
     &.ontap {
         &:before {
             opacity: var(--circle-opacity);

js/classes/Modal.js
@@ -87,7 +87,7 @@ export default class Modal
         this.#primaryCallback = (callback !== undefined) ? callback : default_callback;
 
         // Bind the callback to the button.
-        Modal.$modalBtnPrimary.addEventListener('click', this.#primaryCallback.bind(this), {
+        Modal.$modalBtnPrimary.addEventListener('trigger', this.#primaryCallback.bind(this), {
             signal: Modal.#listenersAbort.signal
         });
 
@@ -108,7 +108,7 @@ export default class Modal
         this.#secondaryCallback = (callback !== undefined) ? callback : default_callback;
 
         // Bind the callback to the button.
-        Modal.$modalBtnSecondary.addEventListener('click', this.#secondaryCallback.bind(this), {
+        Modal.$modalBtnSecondary.addEventListener('trigger', this.#secondaryCallback.bind(this), {
             signal: Modal.#listenersAbort.signal
         });
 

js/classes/Setting.js
@@ -131,7 +131,7 @@ export default class Setting extends HTMLElement
     getResetButtonHTML()
     {
         return (this.allow_reset) ? `
-            <g-icon class="reset-btn" data-name="restore"></g-icon>
+            <g-icon class="reset-btn" data-name="restore" data-tappable="click no-circle"></g-icon>
         ` : '';
     }
 

js/components/SettingRange.js
@@ -133,7 +133,7 @@ export default class SettingRange extends Setting
     {
         // Add the class and the tappable options.
         this.classList.add('setting');
-        this.dataset.tappable = 'follow-tap trigger-manually';
+        this.dataset.tappable = 'trigger-manually follow-tap';
 
         // Set the content.
         this.innerHTML = `

commit 38f8db2f64b74ae7164eff064b993697a61ac71a

fix(settings): stop event propagation from tappable elements
This fixes an issue introduced in commit 0d35a98.

Event bubbling from the reset button to the setting itself was
initially blocked inside the `click` listener of the button. However,
this stopped working after using the `trigger` custom event instead.

Event bubbling is now blocked on all tappable elements, both as a fix
and as a preventive measure to avoid possible future issues.

index.html
@@ -265,7 +265,7 @@
                 ></setting-range>
 
                 <setting-list
-                    data-name="frequency_weighting"
+                    data-name="frequency_weighting" data-allow-reset
                     data-title="Pondération fréquentielle">
                         <select-item data-value="A" data-selected>dB(A)</select-item>
                         <select-item data-value="B">dB(B)</select-item>
@@ -396,13 +396,13 @@
                 ></setting-switch>
 
                 <setting-switch
-                    data-name="keep_screen_awake_at_run" data-value=true data-allow-reset
+                    data-name="keep_screen_awake_at_run" data-value=true
                     data-title="Garder l'écran allumé si actif"
                     data-info="Empêche l'extinction de l'écran si l'onglet affiché est actif."
                 ></setting-switch>
 
                 <setting-switch
-                    data-name="always_keep_screen_awake" data-value=true data-allow-reset
+                    data-name="always_keep_screen_awake" data-value=false
                     data-title="Toujours garder l'écran allumé"
                     data-info="Empêche l'extinction de l'écran tant que l'application est ouverte."
                 ></setting-switch>

js/modules/settings.js
@@ -337,7 +337,6 @@ export function __init__({})
         $resetBtn.addEventListener('trigger', function (event) {
             const $setting = this.closest('.setting');
             $setting.reset();
-            event.stopPropagation()
         });
     });
 

js/modules/utils/tappable.js
@@ -93,5 +93,6 @@ for (const $tappable of $tappables) {
         if (!this.is('trigger-manually')) {
             this.triggerTappable(event);
         }
+        event.stopPropagation();
     });
 }