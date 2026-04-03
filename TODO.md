0d696031cf4b1a4dedb6fc2223807f653a5de841
Jonathan Macioszczyk <jonathan.macioszczyk@gmail.com>
2026-04-03 21:31:47 +0200

    feat: add active tab indicator to nav menu

--- TODO.md
+++ TODO.md
@@ -63,6 +63,7 @@
 - [ ] **Style :** Changer l'icone play, pause et stop à l'intérieur des boutons en les slidant vers le haut ou le bas ?
 - [ ] Notification Push lorsque l'application est mise en arrière-plan pour prévenir de la mise en pause, en plus du Toast ? (Service Worker nécessaire ?)
 - [ ] Fixer un seul bouton play et les boutons secondaires en bas pour toutes les vues.
+- [ ] Utiliser la méthode BEM pour les noms de classes ?
 
 ---
 
--- js/components/nav-tab.js
+++ js/components/nav-tab.js
@@ -3,6 +3,7 @@ import * as Settings from '/js/views/settings.js';
 
 export const $nav = document.querySelector('nav');
 export const $tabs = $nav.querySelectorAll('.nav-tab');
+export const $activeTabIndicator = $nav.querySelector('.active-tab-indicator');
 
 /**
  * Get the tab that loads a specific view.
@@ -43,7 +44,16 @@ function setTabVisibility(view_id, is_visible)
     }
 
     // Display a message if no tab is visible.
-    $nav.querySelector('p.no-tab').classList.toggle('show', (View.getFirstVisible() === null));
+    $nav.querySelector('p.no-tab').classList.toggle('show', (countVisible() === 0));
+}
+
+/**
+ * Get the number of visible tabs.
+ * @returns {number}
+ */
+function countVisible()
+{
+    return $nav.querySelectorAll('.nav-tab:not(.hide)').length;
 }
 
 /**
@@ -61,6 +71,19 @@ export function __init__() {
             setTabVisibility(view_id, event.detail.value);
         });
     };
+
+    // Move the active tab indicator when a view is loaded.
+    View.$main.addEventListener('load', event => {
+        const $tab = getTabFromViewId(event.detail.$view.id);
+        $activeTabIndicator.classList.toggle('show', ($tab !== null));
+
+        // If the view loaded is linked to a nav tab.
+        if ($tab !== null) {
+            $activeTabIndicator.style.translate = $tab.offsetLeft + 'px 0';
+            $activeTabIndicator.style.width = $tab.offsetWidth + 'px';
+            $activeTabIndicator.addClassTemporarily('move', 'animationend');
+        }
+    });
 
     // Run the tab if its view is running.
     View.$main.addEventListener('run', event => {
